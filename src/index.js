ddf3a0129bd63b4de08a6a12b520ad6ff5a2c937
wbh1328551759 <1328551759@qq.com>
2020-05-24 00:24:50 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import x from "./x.js";
+import png from "./assets/add.png";
 console.log(x);
+console.log(png);
+
+const div = document.querySelector("#add");
+div.innerHTML = `
+    <img src="${png}">
+`;
+
+const button = document.createElement("button");
+button.innerHTML = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {}
+  );
+};
+
+div.appendChild(button);
